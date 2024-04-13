a0d6b8b989eed2a78eff74eab7c340ed1598fe1c
Siim Aarmaa <51194757+siimaarmaa@users.noreply.github.com>
2024-04-13 19:03:16 +0300

    Update function.php

--- function.php
+++ function.php
@@ -1,9 +1,15 @@
+<?php
+/*
+  Plugin Name: WordPress E-mail default sender changer by Aarmaa - aarmaa.ee
+  Description: Changes Wordpress default sender address.
+*/
+
 // Please edit the address and name below.
 // Change the From address.
 add_filter( 'wp_mail_from', function ( $original_email_address ) {
     return 'no-reply@aarmaa.ee';
 } );
- 
+
 // Change the From name.
 add_filter( 'wp_mail_from_name', function ( $original_email_from ) {
     return 'Aarmaa';
